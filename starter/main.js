691761334129407f707305a8d0320f4304916bf9
Алексей <agragregra@ya.ru>
2025-11-12 19:04:40 +0300

    Update

--- starter/main.js
+++ starter/main.js
@@ -1,1 +1,10 @@
-// document.body.hidden = true // test
+import Lenis from './vendor/lenis/lenis.mjs'
+
+const lenis = new Lenis()
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
+
+
